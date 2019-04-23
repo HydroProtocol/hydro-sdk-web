59063c95b246caaa4a9a0f05b70285bceb9a3186
wanglei <wanglei@ddex.io>
2019-04-23 21:53:08 +0800

    update runtime env

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,7 +1,13 @@
-const _env = typeof window !== 'undefined' && window._env ? window._env : process.env;
+let _env = process.env;
+
+if (!_env.REACT_APP_API_URL) {
+  _env = window._env;
+}
+
 export default {
   API_ADDRESS: _env.REACT_APP_API_URL,
   WS_ADDRESS: _env.REACT_APP_WS_URL,
   HYDRO_PROXY_ADDRESS: _env.REACT_APP_HYDRO_PROXY_ADDRESS,
   HYDRO_TOKEN_ADDRESS: _env.REACT_APP_HYDRO_TOKEN_ADDRESS
 };
+
